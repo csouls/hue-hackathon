ce77597955831caacc1cb84f0a68515098c39eb5
Naoki Morita <namorit@gmail.com>
2014-03-22 17:50:39 +0900

    ViewControllerを途中まで作成しました。

---
+++ AwesomeLightningMachikon/AwesomeLightningMachikon/Controllers/ALMQuestionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ALMQuestionViewController.h
+//  AwesomeLightningMachikon
+//
+//  Created by MORITA NAOKI on 2014/03/22.
+//  Copyright (c) 2014年 machikons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALMQuestionViewController : UIViewController
+
+@end

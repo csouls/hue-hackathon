8462d6a9be70564c5dc6ddba77a239264d6dbbae
Yusuke Tanaka <y.tanaka@aktsk.jp>
2014-03-23 14:42:28 +0900

    Add light pattern

--- alm-server/app/controllers/matches_controller.rb
+++ alm-server/app/controllers/matches_controller.rb
@@ -20,21 +20,45 @@ class MatchesController < ApplicationController
   def lighting(ip, level)
     client = Hue::Client.new
     lights = client.bridges.map { |bridge| bridge.lights if bridge.ip == "192.168.1.156" }.flatten.compact
-    lights.each do |light|
-      light.saturation = 255
-      light.brightness = 255
-    end
-    colors = [0, 25500, 46920, 59160]
 
-    Parallel.each(lights, in_threads: lights.count) do |light|
-      8.times do |i|
+    case level
+    when 1..2
+      lights.each do |light|
         light.on = true
-        index = (light.id.to_i + i) % 4
-        puts index
-        light.hue = colors[index]
-        sleep 0.2
+        sleep 0.1
+        light.hue = 46920
+        light.saturation = 200
+        light.brightness = 150
       end
-      light.on = false
+      sleep 1.5
+      lights.each { |light| light.on = false }
+    when 3..4
+      lights.each do |light|
+        light.on = true
+        sleep 0.1
+        light.hue = 56100
+        light.saturation = 200
+        light.brightness = 200
+      end
+      sleep 1.5
+      lights.each { |light| light.on = false }
+    when 5
+      lights.each do |light|
+        light.saturation = 255
+        light.brightness = 255
+      end
+      colors = [0, 25500, 46920, 59160]
+
+      Parallel.each(lights, in_threads: lights.count) do |light|
+        8.times do |i|
+          light.on = true
+          index = (light.id.to_i + i) % 4
+          puts index
+          light.hue = colors[index]
+          sleep 0.2
+        end
+        light.on = false
+      end
     end
   end
 end
